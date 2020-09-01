4944cd1d2c774bb6649ade449b351c9c30a78e56
Igor Khotin <khotin@gmx.com>
2020-09-01 23:16:50 +0300

    [+] draw grid according to the layout

--- boy-buf.mod/dna/ShipGrid.js
+++ boy-buf.mod/dna/ShipGrid.js
@@ -1,7 +1,17 @@
+const df = {
+    x: 0,
+    y: 0,
+}
+
 class ShipGrid {
 
     constructor(st) {
+        augment(this, df)
         augment(this, st)
+        if (!this.layout) {
+            // set default
+            this.layout = dna.spec.layout.basic
+        }
     }
 
     draw() {
@@ -10,12 +20,13 @@ class ShipGrid {
         
         stroke(env.style.color.fg0)
         lineWidth(1)
-        alpha(1)
 
         const s = 14
         for (let y = 0; y < 7; y++) {
             for (let x = 0; x < 5; x++) {
-                rect(x*s, y*s, s, s)
+                if (this.layout[y][x] > 0) {
+                    rect(x*s, y*s, s, s)
+                }
             }
         }
         restore()
--- boy-buf.mod/setup.js
+++ boy-buf.mod/setup.js
@@ -1,16 +1,24 @@
 module.exports = function() {
+    _$.boy = _ // mix shortcut
+
     // setup gameboy screen resolution
     ctx.width = _._$.env.tune.width
     ctx.height = _._$.env.tune.height
     ctx.canvas.width = _._$.env.tune.width
     ctx.canvas.height = _._$.env.tune.height
 
-    lab.spawn(dna.ShipGrid, {
+    // create ship grids
+    const left = lab.spawn(dna.ShipGrid, {
+        name: 'left',
         x: 6,
         y: 12,
     })
-    lab.spawn(dna.ShipGrid, {
+
+    const right = lab.spawn(dna.ShipGrid, {
+        name: 'right',
         x: 86,
         y: 12,
+        layout: dna.spec.layout.whale,
     })
+    //right.layout = dna.spec.layout.whale
 }
