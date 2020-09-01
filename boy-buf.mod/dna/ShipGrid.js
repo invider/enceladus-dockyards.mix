083a602d341f715669c53d29bab217f00e6c18d0
Igor Khotin <khotin@gmx.com>
2020-09-02 00:06:41 +0300

    [+] cell targeting

--- boy-buf.mod/dna/ShipGrid.js
+++ boy-buf.mod/dna/ShipGrid.js
@@ -19,10 +19,19 @@ class ShipGrid {
     }
 
     activate(action) {
-        log('do #' + action)
-
         let nx = this.target.x
         let ny = this.target.y
+        switch(action) {
+            case 1: ny--; break;
+            case 2: nx--; break;
+            case 3: ny++; break;
+            case 4: nx++; break;
+        }
+
+        if (this.layout[ny] && this.layout[ny][nx] > 0) {
+            this.target.x = nx
+            this.target.y = ny
+        }
     }
 
     act(action) {
@@ -35,16 +44,23 @@ class ShipGrid {
         save()
         translate(this.x, this.y)
         
-        stroke(env.style.color.fg0)
         lineWidth(1)
 
         const s = 14
         for (let y = 0; y < 7; y++) {
             for (let x = 0; x < 5; x++) {
                 if (this.layout[y][x] > 0) {
+                    stroke(env.style.color.bg1)
                     rect(x*s, y*s, s, s)
                 }
             }
+        }
+
+        if (this.player) {
+            const x = this.target.x
+            const y = this.target.y
+            stroke(env.style.color.fg0)
+            rect(x*s, y*s, s, s)
         }
         restore()
     }
