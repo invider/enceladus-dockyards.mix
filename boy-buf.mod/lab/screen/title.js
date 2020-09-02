3c4bd2286fb88ecc2463dee3d361a345acbf18c9
Igor Khotin <khotin@gmx.com>
2020-09-02 19:39:10 +0300

    [+] title screen

---
+++ boy-buf.mod/lab/screen/title.js
@@ -0,0 +1,29 @@
+const Z = 21
+
+function show() {
+    this.hidden = false
+}
+
+function hide() {
+    this.hidden = true
+}
+
+function draw() {
+    alignCenter()
+    baseMiddle()
+    fill(env.style.color.c3)
+    font('8px gameboy')
+    text('Enceladus Dockyards', rx(.5), ry(.5))
+}
+
+function activate(action) {
+    lab.vfx.transit({
+        fadein: 1,
+        hold: .5,
+        onFadeOut: function() {
+            lab.screen.title.hide()
+            lab.screen.battle.show()
+        },
+        fadeout: 2,
+    })
+}
