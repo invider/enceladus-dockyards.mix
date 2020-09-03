4a6276f7dbc1d8108e9beb0ec7ce09bbf36db3f1
Igor Khotin <khotin@gmx.com>
2020-09-03 23:52:57 +0300

    [+] MassDriver

---
+++ boy-buf.mod/dna/zpods/MassDriver.js
@@ -0,0 +1,9 @@
+class MassDriver extends dna.Pod {
+
+    constructor(st) {
+        super(st)
+        this.name = 'massDriver'
+        this.title = 'Mass Driver'
+        this.cost = 200
+    }
+}
