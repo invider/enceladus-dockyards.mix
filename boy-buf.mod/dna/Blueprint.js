37eac2e77ca95453fb50448a263000dc93aced73
Igor Khotin <khotin@gmx.com>
2020-09-03 12:09:50 +0300

    [*] basic Blueprint implementation

--- boy-buf.mod/dna/Blueprint.js
+++ boy-buf.mod/dna/Blueprint.js
@@ -1,2 +1,11 @@
 class Blueprint {
+
+    constructor(st) {
+        augment(this, st)
+        if (!this.layout) this.layout = dna.spec.layout.basic
+    }
+
+    cellType(x, y) {
+        return this.layout[y][x]
+    }
 }
