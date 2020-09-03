7ef4b25e232a132917b89bcd3c0433a92a8dbccb
Igor Khotin <khotin@gmx.com>
2020-09-03 23:57:45 +0300

    [+] ShieldGenerator

--- boy-buf.mod/dna/zpods/KineticShell.js
+++ boy-buf.mod/dna/zpods/KineticShell.js
@@ -3,7 +3,7 @@ class KineticShell extends dna.Pod {
     constructor(st) {
         super(st)
         this.name = 'kinetic'
-        this.title = 'Kinetic Shells'
+        this.title = 'Kinetic Shell'
         this.cost = 25
     }
 }
---
+++ boy-buf.mod/dna/zpods/ShieldGenerator.js
@@ -0,0 +1,9 @@
+class ShieldGenerator extends dna.Pod {
+
+    constructor(st) {
+        super(st)
+        this.name = 'shieldGen'
+        this.title = 'Shield Gen'
+        this.cost = 75
+    }
+}
