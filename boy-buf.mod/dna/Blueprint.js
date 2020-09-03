240271c318b5eefe739e3b1f19c02e2563b3c456
Igor Khotin <khotin@gmx.com>
2020-09-03 22:51:18 +0300

    [+] blueprint grid

--- boy-buf.mod/dna/Blueprint.js
+++ boy-buf.mod/dna/Blueprint.js
@@ -3,8 +3,30 @@ class Blueprint {
     constructor(st) {
         augment(this, st)
         if (!this.layout) this.layout = dna.spec.layout.basic
+        if (!this.grid) this.fillGrid()
         if (!this.name) this.name = this.layout.name
     }
+
+    fillGrid() {
+        const h = this.layout.length
+        const w = this.layout[0].length
+        const grid = []
+
+        for (let y = 0; y < h; y++) {
+            for (let x = 0; x < w; x++) {
+                const type = this.layout[y][x]
+
+                let cell = 'x'
+                if (type === 1) cell = 'shell'
+                else if (type === 2) cell = 'free'
+                grid[y*w + x] = cell
+            }
+        }
+        this.w = w
+        this.h = h
+        this.grid = grid
+    }
+
 
     estimateCost() {
         if (!this.cost) {
@@ -22,5 +44,13 @@ class Blueprint {
     cellType(x, y) {
         if (!this.layout[y]) return 0
         return this.layout[y][x]
+    }
+
+    podAt(x, y) {
+
+    }
+
+    placePod(x, y, pod) {
+
     }
 }
