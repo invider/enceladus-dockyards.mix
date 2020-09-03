652f599577e82f43d0de4467c056bd0574795ea7
Igor Khotin <khotin@gmx.com>
2020-09-03 23:54:59 +0300

    [+] KineticShell

---
+++ boy-buf.mod/dna/zpods/KineticShell.js
@@ -0,0 +1,9 @@
+class KineticShell extends dna.Pod {
+
+    constructor(st) {
+        super(st)
+        this.name = 'kinetic'
+        this.title = 'Kinetic Shells'
+        this.cost = 25
+    }
+}
