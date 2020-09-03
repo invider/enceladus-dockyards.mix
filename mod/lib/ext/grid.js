5a36dd706073b355696a402f29f46d65b4ee5595
Igor Khotin <khotin@gmx.com>
2020-09-03 11:14:31 +0300

    [+] properties parsing in *.grid, layout cost

--- mod/lib/ext/grid.js
+++ mod/lib/ext/grid.js
@@ -8,14 +8,28 @@ function grid(src) {
         l = l.trim()
         if (l.length === 0 || l.startsWith('#')) return
 
-        const row = []
-        for (let x = 0; x < l.length; x++) {
-            const c = l.charAt(x).toLowerCase()
-            if (c === 'x') row[x] = 1 // external shell
-            else if (c === '*') row[x] = 2 // internal hull
-            else row[x] = 0
+        if (l.startsWith('>')) {
+            l = l.substring(1)
+            const parts = l.split(':')
+            const key = parts[0].trim()
+            let val = parts[1] || true
+            if (isString(val)) {
+                val = val.trim()
+                const ival = parseInt(val)
+                if (!isNaN(ival)) val = ival
+            }
+            layout[key] = val
+
+        } else {
+            const row = []
+            for (let x = 0; x < l.length; x++) {
+                const c = l.charAt(x).toLowerCase()
+                if (c === 'x') row[x] = 1 // external shell
+                else if (c === '*') row[x] = 2 // internal hull
+                else row[x] = 0
+            }
+            layout[y++] = row
         }
-        layout[y++] = row
     })
 
     return layout
