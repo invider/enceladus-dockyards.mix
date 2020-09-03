bb15a596f1164e887c4b1ecc34bfc9ef9ebd67eb
Igor Khotin <khotin@gmx.com>
2020-09-03 20:57:03 +0300

    [+] Pod

--- boy-buf.mod/dna/Pod.js
+++ boy-buf.mod/dna/Pod.js
@@ -1,1 +1,28 @@
+const df = {
+    name: 'pod',
+    hits: 100,
+}
 
+class Pod {
+
+    constructor(st) {
+        augment(this, df)
+        augment(this, st)
+    }
+
+    activate() {}
+
+    turn() {}
+
+    hit(attack) {
+        this.hits -= attack
+        if (this.hits < 0) {
+            this.hits = 0
+            this.kill()
+        }
+    }
+
+    kill() {
+        this.dead = true
+    }
+}
