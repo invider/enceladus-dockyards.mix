5a7dd1ab50f383e85a4cdc5f937bce67e87ce2f4
Igor Khotin <khotin@gmx.com>
2020-09-04 15:24:46 +0300

    [*] init() function

--- boy-buf.mod/dna/zpods/gen.js
+++ boy-buf.mod/dna/zpods/gen.js
@@ -1,9 +1,21 @@
+const df = {
+    name: 'gen',
+    title: 'Shield Gen',
+    cost: 75,
+    hits: 20,
+    charge: 0,
+    maxCharge: 100,
+}
+
 class gen extends dna.Pod {
 
     constructor(st) {
         super(st)
-        this.name = 'gen'
-        this.title = 'Shield Gen'
-        this.cost = 75
+        augment(this, df)
+    }
+
+    init() {
+        this.charge = this.maxCharge
+        log('charging shields in ' + this.ship.name)
     }
 }
