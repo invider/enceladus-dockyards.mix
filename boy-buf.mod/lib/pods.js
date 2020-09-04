87ae55647b783e96dcaf25bc5f7f88dfec19adf0
Igor Khotin <khotin@gmx.com>
2020-09-04 21:31:14 +0300

    [+] pods utilities

---
+++ boy-buf.mod/lib/pods.js
@@ -0,0 +1,26 @@
+function populate() {
+    const map = {}
+    const pods = []
+
+    dna.zpods._ls.forEach(podCons => {
+        const pod = new podCons()
+        pods.push(pod)
+        map[pod.name] = pod
+    })
+
+    this.map = map
+    this.pods = pods
+}
+
+function podPrice(name) {
+    const pod = this.map[name]
+    if (pod && pod.cost) return pod.cost
+    return 0
+}
+
+function podHits(name) {
+    const pod = this.map[name]
+    if (pod && pod.hits) return pod.hits
+    return 0
+}
+
