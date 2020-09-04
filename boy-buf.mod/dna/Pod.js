b302f8c3022f97f134ebfaea3b8133011e58c152
Igor Khotin <khotin@gmx.com>
2020-09-05 00:50:29 +0300

    [+] universal pod recharge()

--- boy-buf.mod/dna/Pod.js
+++ boy-buf.mod/dna/Pod.js
@@ -16,6 +16,23 @@ class Pod {
 
     turn() {}
 
+    recharge(capacity) {
+        if (!this.df || !this.df.charge) return 0
+
+        const need = this.df.charge - this.charge
+        if (need > 0 && capacity > 0) {
+            if (need > capacity) {
+                // take it all!
+                this.charge += capacity
+                return capacity
+            } else {
+                this.charge += need
+                return need
+            }
+        }
+        return 0
+    }
+
     hit(attack) {
         this.hits -= attack
         if (this.hits < 0) {
