89eb66bf6ebf8b839b172208e404edb0e95a5a84
Igor Khotin <khotin@gmx.com>
2020-09-04 23:45:26 +0300

    [+] assemble stat for pods

--- boy-buf.mod/lib/pods.js
+++ boy-buf.mod/lib/pods.js
@@ -1,11 +1,20 @@
 function populate() {
     const map = {}
+    const stat = {}
     const pods = []
 
     dna.zpods._ls.forEach(podCons => {
         const pod = new podCons()
         pods.push(pod)
         map[pod.name] = pod
+
+        const st = []
+        st.push(`cost: $${pod.cost}`)
+        st.push(`hits: ${pod.hits}`)
+        if (pod.attack) st.push(`attack: ${pod.attack}`)
+        if (pod.shots) st.push(`shots: ${pod.shots}`)
+        if (pod.charge) st.push(`charge: ${pod.charge}`)
+        stat[pod.name] = st
     })
 
     this.map = map
@@ -22,5 +31,10 @@ function podHits(name) {
     const pod = this.map[name]
     if (pod && pod.hits) return pod.hits
     return 0
+}
+
+function podStat(name) {
+    const stat = this.stat[name]
+    return stat || []
 }
 
